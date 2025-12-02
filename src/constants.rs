9fc63dc85aecec8c11c6e6b1c368394685b6819a
choithuti <141320770+choithuti@users.noreply.github.com>
2025-12-02 16:40:37 +0700

    Create constants.rs

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+// src/constants.rs
+pub const GENESIS_SIZE: u64 = 4089; //
+pub const AIR_GAP_SIZE: u64 = 8185; //
+pub const FEEDBACK_TIMEOUT_MS: u64 = 493; //
+pub const ETERNAL_SIGNATURE: [u8; 7] = [7, 7, 7, 7, 7, 7, 7]; //
+pub const FORBIDDEN_GENES: [u64; 7] = [7, 77, 777, 7777, 77777, 777777, 7777777]; //
+pub const GHOST_CELL_DEATH: u64 = 220_752_000; // 7 năm giây
